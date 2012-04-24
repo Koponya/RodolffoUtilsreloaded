ada5cc37716c6a454ca146f9a07b27edc401de4b
Zolli <zolli07@gmail.com>
2012-04-24 18:47:33 +0200

    *Added: Fake chat

--- com/zolli/rodolffoutilsreloaded/listeners/commandExecutor.java
+++ com/zolli/rodolffoutilsreloaded/listeners/commandExecutor.java
@@ -52,6 +52,49 @@ public class commandExecutor implements CommandExecutor {
 			return true;
 		}
 		
+		if(command.getName().equalsIgnoreCase("fakechat") || command.getName().equalsIgnoreCase("/fc")) {
+			
+			Player p = null;
+			String message = tu.arrayToString(args, 1);
+			
+			if(sender.isOp() || plugin.perm.has(sender, "rur.fakeChat")) {
+			
+				if(args.length < 2) {
+					return false;
+				}
+				
+				if(Bukkit.matchPlayer(args[0]) == null) {
+					sender.sendMessage("Nem található játékos ezen a néven!");
+				} else {
+					p = plugin.getServer().getPlayer(args[0]);
+				}
+				
+				if(!plugin.perm.has(p, "rur.fakeChat.exempt") && p != null) {
+				
+					for(Player pl : Bukkit.getServer().getOnlinePlayers()) {
+						
+						if(plugin.perm.has(pl, "rur.fakeChat")) {
+							
+							pl.sendMessage(pl.getName().toString() + " írta:");
+							
+						}
+						
+					}
+					
+					p.chat(message);
+				
+				} else {
+					sender.sendMessage("Nem beszélhetsz ezen játékos nevében!");
+				}
+				
+			} else {
+				return false;
+			}
+			
+			return true;
+			
+		}
+		
 		return false;
 	}
 
