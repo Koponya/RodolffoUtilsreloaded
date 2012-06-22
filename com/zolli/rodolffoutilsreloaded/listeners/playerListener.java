71d646eff86a0a7a9a8cdf1fa88d9c25b1180156
Koponya <gaga1004@gmail.com>
2012-06-22 23:54:50 +0200

    fix the Merge error

--- com/zolli/rodolffoutilsreloaded/listeners/playerListener.java
+++ com/zolli/rodolffoutilsreloaded/listeners/playerListener.java
@@ -285,41 +285,45 @@ public class playerListener implements Listener {
 	}
 	
 	@EventHandler(priority=EventPriority.NORMAL)
-	public void getSlimeChunk(PlayerInteractEvent e) {
-		
-		Player pl = e.getPlayer();
-		ItemStack handItem = e.getPlayer().getItemInHand();
-		int handAmount = handItem.getAmount();
-		int newHandAmount = handAmount - 1;
-		long seedCode = pl.getLocation().getWorld().getSeed();
-		Chunk playerOnChunk = pl.getWorld().getChunkAt(e.getClickedBlock());
-		int chunkX = playerOnChunk.getX();
-		int chunkZ = playerOnChunk.getZ();
-		
-		if((e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) && (handItem.getTypeId() == plugin.config.getInt("slimeChunkItem")) && (e.getClickedBlock() != null && e.getAction() != null)) {
-			
-			if(handAmount == 1) {
-				pl.setItemInHand(null);
-			} else {
-				handItem.setAmount(newHandAmount);
-			}
-			
-			Random rand = new Random(seedCode + chunkX * chunkX * 4987142 + chunkX * 5947611 + 
-									chunkZ * chunkZ * 4392871L + chunkZ * 389711 ^ 0x3AD8025F);
-			
-			if(rand.nextInt(10) == 0) {
-				
-				pl.sendMessage(plugin.messages.getString("slimeChunk.isSlimeChunk"));
-				
-			} else {
-				
-				pl.sendMessage(plugin.messages.getString("slimeChunk.isNotSlimeChunk"));
-				
-			}
-			
-		}
-		
-	}
+	  public void getSlimeChunk(PlayerInteractEvent e) {
+	    
+	    if((e.getClickedBlock() != null)) {
+	    
+	      Player pl = e.getPlayer();
+	      ItemStack handItem = e.getPlayer().getItemInHand();
+	      int handAmount = handItem.getAmount();
+	      int newHandAmount = handAmount - 1;
+	      long seedCode = pl.getLocation().getWorld().getSeed();
+	      Chunk playerOnChunk = pl.getWorld().getChunkAt(e.getClickedBlock());
+	      int chunkX = playerOnChunk.getX();
+	      int chunkZ = playerOnChunk.getZ();
+	      
+	      if((e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) && (handItem.getTypeId() == plugin.config.getInt("slimeChunkItem")) && (e.getClickedBlock() != null && e.getAction() != null)) {
+	        
+	        if(handAmount == 1) {
+	          pl.setItemInHand(null);
+	        } else {
+	          handItem.setAmount(newHandAmount);
+	        }
+	        
+	        Random rand = new Random(seedCode + chunkX * chunkX * 4987142 + chunkX * 5947611 + 
+	                    chunkZ * chunkZ * 4392871L + chunkZ * 389711 ^ 0x3AD8025F);
+	        
+	        if(rand.nextInt(10) == 0) {
+	          
+	          pl.sendMessage(plugin.messages.getString("slimeChunk.isSlimeChunk"));
+	          
+	        } else {
+	          
+	          pl.sendMessage(plugin.messages.getString("slimeChunk.isNotSlimeChunk"));
+	          
+	        }
+	        
+	      }
+	    
+	    }
+	    
+	  }
 	
 	
 	@EventHandler(priority=EventPriority.NORMAL)
